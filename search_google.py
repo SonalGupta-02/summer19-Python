14c292971c040daaeceb05081b2d3524c7558c82
SonalGupta-02 <gupta02.sonal@gmail.com>
2019-06-10 20:38:20 +0530

    Update search_google.py

--- search_google.py
+++ search_google.py
@@ -7,5 +7,5 @@ f_n=open("link",'w')
 web= input("Enter the topic to search:--")
 topic=search(web,stop=10)
 for i in topic:
-	f.write(i + "\n")
+	f_n.write(i + "\n")
 webbrowser.open("https://www.google.com/search?q=",topic)
