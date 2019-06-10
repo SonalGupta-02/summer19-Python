92fdcf0b8eb870d5e12078e29ebaf048c58cc8da
SonalGupta-02 <gupta02.sonal@gmail.com>
2019-06-10 20:33:46 +0530

    Create search_google.py

---
+++ search_google.py
@@ -0,0 +1,9 @@
+import webbrowser
+from googlesearch import search
+f_n=open("link",'w')
+
+web= input("Enter the topic to search:--")
+topic=search(web,stop=10)
+for i in topic:
+	f.write(i + "\n")
+webbrowser.open("https://www.google.com/search?q=",topic)
