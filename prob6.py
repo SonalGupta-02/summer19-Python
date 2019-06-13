994710f5427785226be2819111fba9656c3acb56
SonalGupta-02 <gupta02.sonal@gmail.com>
2019-06-14 00:22:26 +0530

    Update prob6.py

--- prob6.py
+++ prob6.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 
-print("1.To print the data of single file \n 2.To print the data of Multiple files \n 3.To create a new Empty file")
+print("1.To print the data of single file \n 2.To print the data of Multiple files \n 3.To create a new Empty file \n 4.To append one file to other file")
 
 choice=int(input("Enter ypur choice:--"))
 if choice == '1':
@@ -19,9 +19,17 @@ elif choice == '2':
 		f=open(i,'r')
 		print('\n')
 		print(f.read())
-	
-	elif choice == '3':
+elif choice == '3':
 		fname=input("Enter the file name u wanna create:--")
 		n_file=open(fname,'w')
 		n_file.close()
+elif choice == '4':
+	f_nme1=input("Enter file to be read:--")
+	f_nme2=input("Enter file to be appended:--")
+	n_rd=open(f_nme1,'r')
+	data=n_rd.read()
+	n_rd.close()
+	n_wr=open(f_nme2,'a')
+	n_wr.write(data)
+	n_wr.close()
 	
