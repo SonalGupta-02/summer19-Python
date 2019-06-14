74815ab49492c00af90246e917003c07a1dcf6c6
SonalGupta-02 <gupta02.sonal@gmail.com>
2019-06-14 23:57:12 +0530

    Update prob12.py

--- prob12.py
+++ prob12.py
@@ -8,6 +8,9 @@ from googlesearch import search
 
 data=input("Enter the data to be searched:--")
 l_url=[i for i in search(data,stop=3)]
-
+f_name=open('var/www/html/qr_code.html','w')
 for i in range(l_url):
 	url=pyqrcode.create(l_url[i])
+	n=open("var/www/html"+data+str(i)+".svg",'wb')
+	url.svg(n,scale=8)
+	
