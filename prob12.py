ec036afa4b5642d91e83da585b16b210ee229f09
SonalGupta-02 <gupta02.sonal@gmail.com>
2019-06-15 21:45:34 +0530

    Update prob12.py

--- prob12.py
+++ prob12.py
@@ -2,15 +2,19 @@
 
 import os
 import pyqrcode
-import webbrowser
 from pyqrcode import QRcode
 from googlesearch import search
 
 data=input("Enter the data to be searched:--")
-l_url=[i for i in search(data,stop=3)]
-f_name=open('var/www/html/qr_code.html','w')
-for i in range(l_url):
-	url=pyqrcode.create(l_url[i])
-	n=open("var/www/html"+data+str(i)+".svg",'wb')
+l_url=[]
+for i in search(data,stop=3):
+	l_url.append(i)
+	print(i)
+j=0
+for j in range(l_url):
+	url=pyqrcode.create(l_url[j])
+	n=open("qr_cd"+data+str(j)+".svg",'wb')
 	url.svg(n,scale=8)
+	j+=1
+	
 	
